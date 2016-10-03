efdb184fa98a73843502acb1aa2b939159009e08
Colin <colindmccann@gmail.com>
2016-10-03 18:54:17 -0400

    real scaffolding

--- testResetDB.sh
+++ testResetDB.sh
@@ -5,10 +5,10 @@ mongo ck-biology-maria1 --eval "db.users.remove()"
 mongoimport -d ck-biology-maria1 -c users --jsonArray scaffolding/pupils-maria1.json
 
 mongo ck-biology-maria1 --eval "db.terms.remove()"
-mongoimport -d ck-biology-maria1 -c terms --jsonArray scaffolding/terms.json
+mongoimport -d ck-biology-maria1 -c terms --jsonArray scaffolding/terms-maria1.json
 
 mongo ck-biology-maria1 --eval "db.relationships.remove()"
-mongoimport -d ck-biology-maria1 -c relationships --jsonArray scaffolding/relationships.json
+mongoimport -d ck-biology-maria1 -c relationships --jsonArray scaffolding/relationships-maria1.json
 
 mongo ck-biology-maria1 --eval "db.lessons.remove()"
 mongoimport -d ck-biology-maria1 -c lessons --jsonArray scaffolding/lessons.json
