9ffcb47835eca9cfe4eccbf68eaec4f903dd6c95
Colin <colindmccann@gmail.com>
2016-10-09 14:45:42 -0400

    more renaming

--- encoreResetDB.sh
+++ encoreResetDB.sh
@@ -2,16 +2,16 @@
 # run this on the server in the ck-biology directory to clean up the terms after use - for use with encore testing
 
 mongo ck-biology-encore --eval "db.users.remove()"
-mongoimport -d ck-biology-encore -c users --jsonArray scaffolding/pupils-encore.json
+mongoimport -d ck-biology-encore -c users --jsonArray scaffolding/encore-users.json
 
 mongo ck-biology-encore --eval "db.terms.remove()"
-mongoimport -d ck-biology-encore -c terms --jsonArray scaffolding/terms-encore.json
+mongoimport -d ck-biology-encore -c terms --jsonArray scaffolding/encore-terms.json
 
 mongo ck-biology-encore --eval "db.relationships.remove()"
-mongoimport -d ck-biology-encore -c relationships --jsonArray scaffolding/relationships-encore.json
+mongoimport -d ck-biology-encore -c relationships --jsonArray scaffolding/encore-relationships.json
 
 mongo ck-biology-encore --eval "db.articles.remove()"
-mongoimport -d ck-biology-encore -c articles --jsonArray scaffolding/articles-encore.json
+mongoimport -d ck-biology-encore -c articles --jsonArray scaffolding/encore-articles.json
 
 mongo ck-biology-encore --eval "db.lessons.remove()"
 mongoimport -d ck-biology-encore -c lessons --jsonArray scaffolding/lessons-unit1.json
