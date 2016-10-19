69d04ad292bb887b7006c4fbf734f710fbbb25ab
Colin <colindmccann@gmail.com>
2016-10-19 19:08:29 -0400

    updated reset script

--- UTSResetDB.sh
+++ UTSResetDB.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 # run this on the server in the ck-biology directory to clean up the terms after use
 
-mongo ck-biology-SBI4UE-01-U1 --eval "db.users.remove()"
-mongoimport -d ck-biology-SBI4UE-01-U1 -c users --jsonArray scaffolding/SBI4UE-01-U1-users.json
+mongo ck-biology-SBI4UE-01-U2 --eval "db.users.remove()"
+mongoimport -d ck-biology-SBI4UE-01-U2 -c users --jsonArray scaffolding/SBI4UE-01-U2-users.json
 
-mongo ck-biology-SBI4UE-01-U1 --eval "db.terms.remove()"
-mongoimport -d ck-biology-SBI4UE-01-U1 -c terms --jsonArray scaffolding/SBI4UE-01-U1-terms.json
+mongo ck-biology-SBI4UE-01-U2 --eval "db.terms.remove()"
+mongoimport -d ck-biology-SBI4UE-01-U2 -c terms --jsonArray scaffolding/SBI4UE-01-U2-terms.json
 
-mongo ck-biology-SBI4UE-01-U1 --eval "db.relationships.remove()"
-mongoimport -d ck-biology-SBI4UE-01-U1 -c relationships --jsonArray scaffolding/SBI4UE-01-U1-relationships.json
+mongo ck-biology-SBI4UE-01-U2 --eval "db.relationships.remove()"
+mongoimport -d ck-biology-SBI4UE-01-U2 -c relationships --jsonArray scaffolding/SBI4UE-01-U2-relationships.json
 
-mongo ck-biology-SBI4UE-01-U1 --eval "db.articles.remove()"
-mongoimport -d ck-biology-SBI4UE-01-U1 -c articles --jsonArray scaffolding/SBI4UE-01-U1-articles.json
+#mongo ck-biology-SBI4UE-01-U2 --eval "db.articles.remove()"
+#mongoimport -d ck-biology-SBI4UE-01-U2 -c articles --jsonArray scaffolding/SBI4UE-01-U2-articles.json
 
-mongo ck-biology-SBI4UE-01-U1 --eval "db.lessons.remove()"
-mongoimport -d ck-biology-SBI4UE-01-U1 -c lessons --jsonArray scaffolding/lessons-unit1.json
+mongo ck-biology-SBI4UE-01-U2 --eval "db.lessons.remove()"
+mongoimport -d ck-biology-SBI4UE-01-U2 -c lessons --jsonArray scaffolding/lessons-unit1.json
 
 
-mongo ck-biology-SBI4UE-02-U1 --eval "db.users.remove()"
-mongoimport -d ck-biology-SBI4UE-02-U1 -c users --jsonArray scaffolding/SBI4UE-02-U1-users.json
+mongo ck-biology-SBI4UE-02-U2 --eval "db.users.remove()"
+mongoimport -d ck-biology-SBI4UE-02-U2 -c users --jsonArray scaffolding/SBI4UE-02-U2-users.json
 
-mongo ck-biology-SBI4UE-02-U1 --eval "db.terms.remove()"
-mongoimport -d ck-biology-SBI4UE-02-U1 -c terms --jsonArray scaffolding/SBI4UE-02-U1-terms.json
+mongo ck-biology-SBI4UE-02-U2 --eval "db.terms.remove()"
+mongoimport -d ck-biology-SBI4UE-02-U2 -c terms --jsonArray scaffolding/SBI4UE-02-U2-terms.json
 
-mongo ck-biology-SBI4UE-02-U1 --eval "db.relationships.remove()"
-mongoimport -d ck-biology-SBI4UE-02-U1 -c relationships --jsonArray scaffolding/SBI4UE-02-U1-relationships.json
+mongo ck-biology-SBI4UE-02-U2 --eval "db.relationships.remove()"
+mongoimport -d ck-biology-SBI4UE-02-U2 -c relationships --jsonArray scaffolding/SBI4UE-02-U2-relationships.json
 
-mongo ck-biology-SBI4UE-02-U1 --eval "db.articles.remove()"
-mongoimport -d ck-biology-SBI4UE-02-U1 -c articles --jsonArray scaffolding/SBI4UE-02-U1-articles.json
+#mongo ck-biology-SBI4UE-02-U2 --eval "db.articles.remove()"
+#mongoimport -d ck-biology-SBI4UE-02-U2 -c articles --jsonArray scaffolding/SBI4UE-02-U2-articles.json
 
-mongo ck-biology-SBI4UE-02-U1 --eval "db.lessons.remove()"
-mongoimport -d ck-biology-SBI4UE-02-U1 -c lessons --jsonArray scaffolding/lessons-unit1.json
+mongo ck-biology-SBI4UE-02-U2 --eval "db.lessons.remove()"
+mongoimport -d ck-biology-SBI4UE-02-U2 -c lessons --jsonArray scaffolding/lessons-unit1.json
