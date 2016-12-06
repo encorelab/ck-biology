a63f11acdafa888b8f764dc8f938e8da7a20b450
Colin <colindmccann@gmail.com>
2016-12-06 16:25:08 -0500

    added assets for u3, text entry now saved as array of entries

--- js/research.view.js
+++ js/research.view.js
@@ -101,14 +101,14 @@
           if (app.getMyGroup(app.username, "review3")) {
             jQuery('#knowledge-base-nav-btn').addClass('hidden');
             jQuery('#contribution-nav-btn').addClass('hidden');
-            jQuery('#review-wizard-screen').removeClass('hidden');
-            if (app.reviewWizardView === null) {
-              app.reviewWizardView = new app.View.ReviewWizardView({
-                el: '#review-wizard-screen'
+            jQuery('#report-screen').removeClass('hidden');
+            if (app.reportView === null) {
+              app.reportView = new app.View.ReportView({
+                el: '#report-screen'
                 //model: Skeletor.Model.awake.articles.findWhere({"field": app.getMyField(app.username)})
               });
             }
-            app.reviewWizardView.render();
+            app.reportView.render();
           } else {
             jQuery().toastmessage('showErrorToast', "You have not been assigned to a team!");
             jQuery('.top-nav-btn').addClass('hidden');
@@ -1956,59 +1956,105 @@
 
   /***********************************************************
    ***********************************************************
-   ****************** REVIEW WIZARD VIEW *********************
+   ********************** REPORT VIEW ************************
    ***********************************************************
    ***********************************************************/
 
   app.report = {
     "group_colour": "red",
-    "source": "/reports/pdfs/cow_burps.pdf",
+    "source": "/reports/pdfs/bovine_methane_proposal.pdf",
     "lesson": "review3",
     "parts": [
       {
         "number": 1,
-        "kind": "intro",
+        "kind": "read",
         "name": "Introduction"
       },
       {
         "number": 2,
-        "kind": "progress",
-        "name": "Explanation of Gene Sequences for Transcription and Translation"
+        "kind": "read",
+        "name": "Research Proposal"
       },
       {
         "number": 3,
-        "kind": "progress",
-        "name": "Transcription and Translation of Mutation"
+        "kind": "read",
+        "name": "The Underlying Science"
+      },
+      {
+        "number": 4,
+        "kind": "write",
+        "name": "Representation of the pmoCAB Operon"
+      },
+      {
+        "number": 5,
+        "kind": "write",
+        "name": "Explanation of Gene Sequences for Transcription and Translation"
+      },
+      {
+        "number": 6,
+        "kind": "write",
+        "name": "Structure of the pmoCAB Operon"
       }
     ]
   }
 
   app.html = [
-    `<h2>Introduction</h2>
+    `
+    <h2>Introduction</h2>
     <p>Cattle farming is a key component of Canadian agriculture. However, approximately 23% of Canada’s methane emissions are from cow burps ("enteric fermentations"). This number is troubling because the Global Warming Potential (GWP) of methane is 21 times that of carbon dioxide on a 100-year time scale (Environmental Protection Agency, 2013). Some progress in methane reduction has been made by scientists who have adjusted the composition of cattle feed (Rathke, 2013), but opportunities exist to pursue more cutting-edge approaches.</p>
-    <p>Maria Niño-Soto, a self-made billionaire, has decided to become a leader in the cattle industry by finding ways to make it more environmentally friendly. Towards this end, she has started the Niño-Soto Foundation (NSF) to fund research projects that could provide new solutions to problems in the cattle industry. You have been hired by the NSF as an expert reviewer, tasked with evaluating a research proposal in order to decide if the proposed project is both possible and scientifically sound. As part of your evaluation, you will prepare a report to Maria Niño-Soto in which you explain key elements of the research and comment on its plausibility.</p>`
-    ,
-    `<h2>Explanation of Gene Sequences for Transcription and Translation</h2>
+    <p>Maria Niño-Soto, a self-made billionaire, has decided to become a leader in the cattle industry by finding ways to make it more environmentally friendly. Towards this end, she has started the Niño-Soto Foundation (NSF) to fund research projects that could provide new solutions to problems in the cattle industry. You have been hired by the NSF as an expert reviewer, tasked with evaluating a research proposal in order to decide if the proposed project is both possible and scientifically sound. As part of your evaluation, you will prepare a report to Maria Niño-Soto in which you explain key elements of the research and comment on its plausibility.</p>
+    `,
+    `
+    <h2>Research Proposal</h2>
+    <object id="attach-terms-pdf-content" type="application/pdf" data="reports/pdfs/bovine_methane_proposal.pdf?#zoom=60&scrollbar=0&toolbar=0&navpanes=0"><p>PDF cannot be displayed</p></object>
+    `,
+    `
+    <h2>The Underlying Science</h2>
+    <p>The enzyme pMMO (particulate methane monooxygenase) is a key part of Dr. Heifer Sutherland’s proposed project, so you will need to understand some basic features of this protein complex and the genes encoding it. You review the literature about pMMO and learn that pMMO is a membrane-bound copper and iron containing enzyme. The structural genes for this enzyme lie in a three-gene operon named <i>pmoCAB</i>. This operon codes for the three integral membrane polypeptides: PmoC, PmoA and PmoB of approximately 23, 27, and 45 kDa, respectively, that make up the final functional enzyme pMMO (McDonald et al., 2008).</p>
+    <p>It is really important that the NSF understands that the genes contain all the information necessary for a cell to produce methane monooxygenase. You decide that your report to the NSF should contain the answers to the following specific questions in order to be able to understand the proposed project and to be able to address questions surrounding its feasibility.</p>
+    `,
+    `
+    <h2>Representation of the <i>pmoCAB</i> Operon</h2>
+    <p>Which diagram would you choose as the most useful and accurate depiction of the arrangement of the genes in the chromosome of <i>M. capsulatus</i>? In the text box below, indicate the letter of your chosen diagram and give reasons to support your choice.</p>
+    <img src="reports/imgs/pmo_genes.png"></img>
+    <textarea placeholder="Enter your text here"></textarea>
+    `,
+    `
+    <h2>Explanation of Gene Sequences for Transcription and Translation</h2>
     <p>A gene includes a number of important sequences that ensure transcription and translation of the gene: Promoter, initiation and termination sequences, RBS, start and stop codons. Briefly describe the role and function of each of these within the <i>M. capsulatus</i> (be sure to indicate whether it is important for transcription or for translation). Since billionaire Ni&ntilde;o-Soto may question why she must understand such detail, indicate why you think it is important to understand in your report to the NSF (i.e. the text box below).</p>
-    <textarea placeholder='Enter your text here'></textarea>`
-    ,
-    `<h2>Transcription and Translation of Mutation</h2>
-    <p>The DNA sequence shown on the previous page is the sense (+) strand of the double stranded DNA that makes up the pmoC gene, encoding PmoC.</p>
-    <p>What would the mRNA sequence be when the "mistake" template (−) strand DNA is transcribed? (Give the first 60 bases).</p>
-    <i>Hint: Recall that the first nucleotide of pmoC1 begins at site 1557.</i>
-    <textarea placeholder='Enter your text here'></textarea>
-    <p>What would the amino acid sequence be when this mRNA is translated? (Give the first 15 amino acids).</p>
-    <textarea placeholder='Enter your text here'></textarea>`
+    <textarea placeholder="Enter your text here"></textarea>
+    `,
+    `
+    <h2>Structure of the <i>pmoCAB</i> Operon</h2>
+    <ul>In the text box below, respond to the following questions using full sentences:
+      <li>1. How many promoters would be necessary to allow transcription of the pmoCAB operon?</li>
+      <li>2. How many mRNA molecules will be produced when this operon is transcribed?</li>
+      <li>3. How many ribosome binding sites will be present on the RNA produced when the pmo operon is transcribed?</li>
+      <li>4. How many start codons will be present on the RNA produced when the pmo operon is transcribed?</li>
+      <li>5. How many stop codons will be present on the RNA produced when the pmo operon is transcribed?</li>
+    </ul>
+    <textarea placeholder="Enter your text here"></textarea>
+    <ul>Use the figure below to respond to the following questions. Type your answers in the text box using full sentences.
+      <li>1. Which letter best marks the area within the DNA sequence where RNA polymerase would bind to ensure transcription of pmoA, pmoB, and pmoC as a unit?</li>
+      <li>2. Which letter best marks the area within the DNA sequence that serves as the initiation site for transcription?</li>
+      <li>3. Which letter best marks the area within the DNA sequence that serves as the termination site for transcription?</li>
+      <li>4. Which letter best marks the area within the DNA sequence that contains sequences encoding a translational start site?</li>
+      <li>5. Which letter best marks the area within the DNA sequence that contains sequences encoding translational stop codons?</li>
+      <li>6. Which letter marks the 5’ end of the DNA strand?</li>
+    </ul>
+    <img src="/reports/imgs/pmo_operon.png"></img>
+    <textarea placeholder="Enter your text here"></textarea>
+    `
   ]
 
-  app.View.ReviewWizardView = Backbone.View.extend({
+  app.View.ReportView = Backbone.View.extend({
     initialize: function() {
-      console.log('Initializing ReviewWizardView...');
+      console.log('Initializing ReportView...');
     },
 
     events: {
-      'click #wizard-step-forward-btn' : 'stepForward',
-      'click #wizard-step-back-btn'    : 'stepBack'
+      'click #report-step-forward-btn' : 'stepForward',
+      'click #report-step-back-btn'    : 'stepBack'
     },
 
     stepForward: function() {
@@ -2016,7 +2062,7 @@
 
       view.updateReport();
 
-      app.currentWizardPage++;
+      app.currentReportPage++;
       view.render();
     },
 
@@ -2025,16 +2071,23 @@
 
       view.updateReport();
 
-      app.currentWizardPage--;
+      app.currentReportPage--;
       view.render();
     },
 
     updateReport: function() {
-      jQuery('#wizard-content-container textarea').each(function(index, el) {
-        // key should be related to how the info will be displayed in the report, instead of entry0, entry1, etc
-        var key = "entry"+index;
-        app.report.parts[app.currentWizardPage - 1][key] = jQuery(el).val();
+      var inputs = [];
+      jQuery('#report-content-container textarea').each(function(index, el) {
+        // key should be related to how the info will be displayed in the report, instead of entry0, entry1, etc. Maybe use class names?
+        //var key = "entry"+index;
+        //app.report.parts[app.currentReportPage - 1][key] = jQuery(el).val();
+
+        // this is wrong too, the entries should be keyed to something. Use objects instead?
+        inputs.push(jQuery(el).val());
+
       });
+
+      app.report.parts[app.currentReportPage - 1].entries = inputs;
     },
 
     // this returns a 'non-updateable' object. Maybe refactor so that this is essentially a getter/setter (or better yet, put this all in the model)
@@ -2050,13 +2103,17 @@
 
     render: function() {
       var view = this;
-      console.log("Rendering ReviewWizardView...");
+      console.log("Rendering ReportView...");
 
-      jQuery('#wizard-content-container').html('');
-
-      var currentPart = view.getReportPart(app.currentWizardPage)
-
-      jQuery('#wizard-content-container').append(app.html[currentPart.number - 1]);
+      jQuery('#report-content-container').html('');
+      // create the html
+      jQuery('#report-content-container').append(app.html[app.currentReportPage - 1]);
+      // add the text entries
+      jQuery('#report-content-container textarea').each(function(index, el) {
+        if (view.getReportPart(app.currentReportPage).entries) {
+          jQuery(el).val(view.getReportPart(app.currentReportPage).entries[index]);
+        }
+      });
     }
   });
 
