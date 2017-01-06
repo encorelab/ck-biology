84eb2431cc0e3f674ce19e6b02769bad64caa249
Colin <colindmccann@gmail.com>
2017-01-05 20:26:17 -0500

    more tame encore reset

--- encoreResetDB.sh
+++ encoreResetDB.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # run this on the server in the ck-biology directory to clean up the terms after use - for use with encore testing
 
-mongo ck-biology-encore --eval "db.users.remove()"
-mongoimport -d ck-biology-encore -c users --jsonArray scaffolding/encore-users.json
+# mongo ck-biology-encore --eval "db.users.remove()"
+# mongoimport -d ck-biology-encore -c users --jsonArray scaffolding/encore-users.json
 
-mongo ck-biology-encore --eval "db.terms.remove()"
-mongoimport -d ck-biology-encore -c terms --jsonArray scaffolding/encore-terms.json
+# mongo ck-biology-encore --eval "db.terms.remove()"
+# mongoimport -d ck-biology-encore -c terms --jsonArray scaffolding/encore-terms.json
 
-mongo ck-biology-encore --eval "db.relationships.remove()"
-mongoimport -d ck-biology-encore -c relationships --jsonArray scaffolding/encore-relationships.json
+# mongo ck-biology-encore --eval "db.relationships.remove()"
+# mongoimport -d ck-biology-encore -c relationships --jsonArray scaffolding/encore-relationships.json
 
 mongo ck-biology-encore --eval "db.articles.remove()"
 mongoimport -d ck-biology-encore -c articles --jsonArray scaffolding/encore-articles.json
