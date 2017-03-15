857b5086a5f417691a487181df3b34c7363ba7d2
megseoh <ohara.meagan@gmail.com>
2017-03-15 12:43:19 -0400

    added neurology content

--- reports/certification-reports.js
+++ reports/certification-reports.js
@@ -78,7 +78,7 @@ Skeletor.Mobile.certificationReports = [
       "thumbnail": "",
       "html": `
         <h2>Nephrology Certification Questions</h2>
-        <p>Working as a team, answer all of the following five questions correctly to earn your certificate of specialization in nephrology.</p>`
+        <p>Working as a team, answer all of the following four questions correctly to earn your certificate of specialization in nephrology.</p>`
     },
     {
       "number": 2,
@@ -94,7 +94,7 @@ Skeletor.Mobile.certificationReports = [
       "number": 3,
       "kind": "write",
       "name": "",
-      "thumbnail": "reports/imgs/immunology_question2.png",
+      "thumbnail": "reports/imgs/nephrology_question2.jpg",
       "question": `Using the diagram below, explain how the kidney maintains homeostasis for blood osmolarity.`,
       "html": `
         <p>Using the diagram below, explain how the kidney maintains homeostasis for blood osmolarity.</p>
@@ -105,7 +105,7 @@ Skeletor.Mobile.certificationReports = [
       "number": 4,
       "kind": "write",
       "name": "",
-      "thumbnail": "",
+      "thumbnail": "reports/imgs/nephrology_question3.jpg",
       "question": `Glomerulonephritis is an inflammation of the tiny filters of the kidney (glomeruli). Glomeruli become inflamed and/or scarred, which decreases the amount of blood-flow to the kidneys and prevents the elimination of waste from the body.  Almost all patients with glomerulonephritis develop hypertension (increased blood pressure), since the decreased blood-flow to the kidneys triggers the activation of the renin-angiotensin-aldosterone system (RAAS).  Use the diagram below to explain how the activation of the RAAS leads to increased blood pressure.`,
       "html": `
         <p>Glomerulonephritis is an inflammation of the tiny filters of the kidney (glomeruli). Glomeruli become inflamed and/or scarred, which decreases the amount of blood-flow to the kidneys and prevents the elimination of waste from the body.  Almost all patients with glomerulonephritis develop hypertension (increased blood pressure), since the decreased blood-flow to the kidneys triggers the activation of the renin-angiotensin-aldosterone system (RAAS).  Use the diagram below to explain how the activation of the RAAS leads to increased blood pressure.</p>
@@ -116,7 +116,7 @@ Skeletor.Mobile.certificationReports = [
       "number": 5,
       "kind": "write",
       "name": "",
-      "thumbnail": "",
+      "thumbnail": "reports/imgs/nephrology_question4.jpg",
       "question": `The images below represent the results of a urinalysis (indicator strip) and urine microscopy for a patient experiencing fatigue, frequent urination, and high blood pressure.  Use the Urinalysis Table and Urine Microscopy Table (handouts) to explain whether or not you would diagnose this patient with glomerulonephritis.`,
       "html": `
         <p>The images below represent the results of a urinalysis (indicator strip) and urine microscopy for a patient experiencing fatigue, frequent urination, and high blood pressure.  Use the Urinalysis Table and Urine Microscopy Table (handouts) to explain whether or not you would diagnose this patient with glomerulonephritis.</p>
@@ -132,63 +132,65 @@ Skeletor.Mobile.certificationReports = [
     {
       "number": 1,
       "kind": "read",
-      "name": "Immunology Certification Questions",
+      "name": "Neurology Certification Questions",
       "thumbnail": "",
       "html": `
-        <h2>Immunology Certification Questions</h2>
-        <p>Working as a team, answer all of the following five questions correctly to earn your certificate of specialization in immunology.</p>`
+        <h2>Neurology Certification Questions</h2>
+        <p>Working as a team, answer all of the following four questions correctly to earn your certificate of specialization in neurology.</p>`
     },
     {
       "number": 2,
       "kind": "write",
       "name": "",
-      "thumbnail": "",
-      "question": `Explain how an inflammatory response works, including how it is triggered. Is this an example of innate immunity or adaptive immunity?`,
+      "thumbnail": "reports/imgs/neurology_question1.png",
+      "question": `Use the diagram below as an example to describe the three main functions of the nervous system (i.e. sensory input, integration, and motor output).`,
       "html": `
-        <p>Explain how an inflammatory response works, including how it is triggered. Is this an example of innate immunity or adaptive immunity?</p>
+        <p>Use the diagram below as an example to describe the three main functions of the nervous system (i.e. sensory input, integration, and motor output).</p>
+        <img src="reports/imgs/neurology_question1.png"></img>
         <textarea placeholder="Enter your text here"></textarea>`
     },
     {
       "number": 3,
       "kind": "write",
       "name": "",
-      "thumbnail": "reports/imgs/immunology_question2.png",
-      "question": `Use the diagram below to explain the role of helper T cells in mobilizing both the humoral and cell-mediated branches of the immune response`,
+      "thumbnail": "reports/imgs/neurology_questions2and3.jpg",
+      "question": `Explain the role of voltage-gated ion channels in each of the 5 stages of an action potential shown in the diagram below.`,
       "html": `
-        <p>Use the diagram below to explain the role of helper T cells in mobilizing both the humoral and cell-mediated branches of the immune response:</p>
-        <img src="reports/imgs/immunology_question2.png"></img>
+        <p>Explain the role of voltage-gated ion channels in each of the 5 stages of an action potential shown in the diagram below.</p>
+        <img src="reports/imgs/neurology_questions2and3.jpg"></img>
         <textarea placeholder="Enter your text here"></textarea>`
     },
     {
       "number": 4,
       "kind": "write",
       "name": "",
-      "thumbnail": "",
-      "question": `How is the immune system able to distinguish self from non-self?  What happens if it fails to do so?`,
+      "thumbnail": "reports/imgs/neurology_questions2and3.jpg",
+      "question": `Generalized epilepsy is an inherited syndrome associated with a mutation in the β1 subunit of voltage-gated sodium channels.  This mutation leads to increased sodium channel availability during periods of neuron hyperpolarization (e.g. undershoot).  The diagram below (also shown in the previous question) represents the 5 stages of an action potential in a healthy/non-epileptic individual.  Explain how the β1 subunit mutation would affect the nerve impulses of an individual with generalized epilepsy.`,
       "html": `
-        <p>How is the immune system able to distinguish self from non-self?  What happens if it fails to do so?</p>
+        <p>Generalized epilepsy is an inherited syndrome associated with a mutation in the β1 subunit of voltage-gated sodium channels.  This mutation leads to increased sodium channel availability during periods of neuron hyperpolarization (e.g. undershoot).  The diagram below (also shown in the previous question) represents the 5 stages of an action potential in a healthy/non-epileptic individual.  Explain how the β1 subunit mutation would affect the nerve impulses of an individual with generalized epilepsy.</p>
+        <img src="reports/imgs/neurology_questions2and3.jpg"></img>
         <textarea placeholder="Enter your text here"></textarea>`
     },
     {
       "number": 5,
       "kind": "write",
       "name": "",
-      "thumbnail": "",
-      "question": `Systemic Lupus Erythematosus (SLE, or "lupus") is an autoimmune disease characterized by the presence of large amounts of autoantibodies (i.e. antibodies that bind to self antigens) that circulate throughout the body.  The autoantibodies in lupus are called "antinuclear antibodies" (ANA) because they bind to the contents of a cell's nucleus.  As shown in the autoantibodies table (handout), there are many subtypes of ANAs, each of which binds to different proteins or protein complexes within the nucleus. According to this table, what kind of ANAs would be found in patients with SLE?`,
+      "thumbnail": "reports/imgs/neurology_question4.jpg",
+      "question": `Neuropsychiatric manifestations of systemic lupus erythematosus (NPSLE) comprise a complex array of neurological, psychiatric and behavioural manifestations. Seizures are one of the most ominous and relevant clinical expressions of damage accrual in SLE.  Seizures occur when a group of neurons begin firing in an abnormal, excessive, and synchronized manner.  An electroencephalography (EEG) may be used to help determine the type of seizure or syndrome present.  An EEG measures voltage fluctuations resulting from ionic current within the neurons of the brain.  The names of the electrode sites use alphabetical abbreviations that identify the lobe or area of the brain which each electrode records from: F = frontal Fp = frontopolar T = temporal C = central P = parietal O = occipital A = auricular (ear electrode). The image below represents EEG results of a patient experiencing NPSLE.  Use the handouts provided to identify the waveform pattern that is present in this image.  Is this pattern generalized or local?  Which hemispheres and/or specific regions of the brain are affected?`,
       "html": `
-        <p>Systemic Lupus Erythematosus (SLE, or "lupus") is an autoimmune disease characterized by the presence of large amounts of autoantibodies (i.e. antibodies that bind to self antigens) that circulate throughout the body.  The autoantibodies in lupus are called "antinuclear antibodies" (ANA) because they bind to the contents of a cell's nucleus.  As shown in the autoantibodies table (handout), there are many subtypes of ANAs, each of which binds to different proteins or protein complexes within the nucleus:</p>
-        <p>According to this table, what kind of ANAs would be found in patients with SLE?</p>
-        <textarea placeholder="Enter your text here"></textarea>`
-    },
-    {
-      "number": 6,
-      "kind": "write",
-      "name": "",
-      "thumbnail": "reports/imgs/immunology_question5.png",
-      "question": `The presence of ANAs in blood can be confirmed using a screening test. Following the positive detection of ANAs, the various subtypes of ANAs can be determined.  Based on the ANA test results shown below, what would be your diagnosis for this patient?`,
-      "html": `
-        <p>The presence of ANAs in blood can be confirmed using a screening test. Following the positive detection of ANAs, the various subtypes of ANAs can be determined.  Based on the ANA test results shown below, what would be your diagnosis for this patient?</p>
-        <img src="reports/imgs/immunology_question5.png"></img>
+        <p>Neuropsychiatric manifestations of systemic lupus erythematosus (NPSLE) comprise a complex array of neurological, psychiatric and behavioural manifestations. Seizures are one of the most ominous and relevant clinical expressions of damage accrual in SLE.  Seizures occur when a group of neurons begin firing in an abnormal, excessive, and synchronized manner.</p>
+        <p>An electroencephalography (EEG) may be used to help determine the type of seizure or syndrome present.  An EEG measures voltage fluctuations resulting from ionic current within the neurons of the brain.  The names of the electrode sites use alphabetical abbreviations that identify the lobe or area of the brain which each electrode records from:</p>
+        <ul>
+          <li>F = frontal</li>
+          <li>Fp = frontopolar</li>
+          <li>T = temporal</li>
+          <li>C = central</li>
+          <li>P = parietal</li>
+          <li>O = occipital</li>
+          <li>A = auricular (ear electrode)</li>
+        </ul>
+        <p>The image below represents EEG results of a patient experiencing NPSLE.  Use the handouts provided to identify the waveform pattern that is present in this image.  Is this pattern generalized or local?  Which hemispheres and/or specific regions of the brain are affected?</p>
+        <img src="reports/imgs/neurology_question4.jpg"></img>
         <textarea placeholder="Enter your text here"></textarea>`
     }
   ]
