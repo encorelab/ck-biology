f8a2a4e4f2241ea70cf10ee58775f62f0436b216
megseoh <ohara.meagan@gmail.com>
2017-03-15 12:24:06 -0400

    added nephrology content

--- reports/certification-reports.js
+++ reports/certification-reports.js
@@ -74,20 +74,20 @@ Skeletor.Mobile.certificationReports = [
     {
       "number": 1,
       "kind": "read",
-      "name": "Immunology Certification Questions",
+      "name": "Nephrology Certification Questions",
       "thumbnail": "",
       "html": `
-        <h2>Immunology Certification Questions</h2>
-        <p>Working as a team, answer all of the following five questions correctly to earn your certificate of specialization in immunology.</p>`
+        <h2>Nephrology Certification Questions</h2>
+        <p>Working as a team, answer all of the following five questions correctly to earn your certificate of specialization in nephrology.</p>`
     },
     {
       "number": 2,
       "kind": "write",
       "name": "",
       "thumbnail": "",
-      "question": `Explain how an inflammatory response works, including how it is triggered. Is this an example of innate immunity or adaptive immunity?`,
+      "question": `Briefly describe the four key functions of the excretory system (i.e. filtration, reabsorption, secretion, and excretion), and identify where each of these processes occurs in humans.`,
       "html": `
-        <p>Explain how an inflammatory response works, including how it is triggered. Is this an example of innate immunity or adaptive immunity?</p>
+        <p>Briefly describe the four key functions of the excretory system (i.e. filtration, reabsorption, secretion, and excretion), and identify where each of these processes occurs in humans.</p>
         <textarea placeholder="Enter your text here"></textarea>`
     },
     {
@@ -95,10 +95,10 @@ Skeletor.Mobile.certificationReports = [
       "kind": "write",
       "name": "",
       "thumbnail": "reports/imgs/immunology_question2.png",
-      "question": `Use the diagram below to explain the role of helper T cells in mobilizing both the humoral and cell-mediated branches of the immune response`,
+      "question": `Using the diagram below, explain how the kidney maintains homeostasis for blood osmolarity.`,
       "html": `
-        <p>Use the diagram below to explain the role of helper T cells in mobilizing both the humoral and cell-mediated branches of the immune response:</p>
-        <img src="reports/imgs/immunology_question2.png"></img>
+        <p>Using the diagram below, explain how the kidney maintains homeostasis for blood osmolarity.</p>
+        <img src="reports/imgs/nephrology_question2.jpg"></img>
         <textarea placeholder="Enter your text here"></textarea>`
     },
     {
@@ -106,9 +106,10 @@ Skeletor.Mobile.certificationReports = [
       "kind": "write",
       "name": "",
       "thumbnail": "",
-      "question": `How is the immune system able to distinguish self from non-self?  What happens if it fails to do so?`,
+      "question": `Glomerulonephritis is an inflammation of the tiny filters of the kidney (glomeruli). Glomeruli become inflamed and/or scarred, which decreases the amount of blood-flow to the kidneys and prevents the elimination of waste from the body.  Almost all patients with glomerulonephritis develop hypertension (increased blood pressure), since the decreased blood-flow to the kidneys triggers the activation of the renin-angiotensin-aldosterone system (RAAS).  Use the diagram below to explain how the activation of the RAAS leads to increased blood pressure.`,
       "html": `
-        <p>How is the immune system able to distinguish self from non-self?  What happens if it fails to do so?</p>
+        <p>Glomerulonephritis is an inflammation of the tiny filters of the kidney (glomeruli). Glomeruli become inflamed and/or scarred, which decreases the amount of blood-flow to the kidneys and prevents the elimination of waste from the body.  Almost all patients with glomerulonephritis develop hypertension (increased blood pressure), since the decreased blood-flow to the kidneys triggers the activation of the renin-angiotensin-aldosterone system (RAAS).  Use the diagram below to explain how the activation of the RAAS leads to increased blood pressure.</p>
+        <img src="reports/imgs/nephrology_question3.jpg"></img>
         <textarea placeholder="Enter your text here"></textarea>`
     },
     {
@@ -116,21 +117,10 @@ Skeletor.Mobile.certificationReports = [
       "kind": "write",
       "name": "",
       "thumbnail": "",
-      "question": `Systemic Lupus Erythematosus (SLE, or "lupus") is an autoimmune disease characterized by the presence of large amounts of autoantibodies (i.e. antibodies that bind to self antigens) that circulate throughout the body.  The autoantibodies in lupus are called "antinuclear antibodies" (ANA) because they bind to the contents of a cell's nucleus.  As shown in the autoantibodies table (handout), there are many subtypes of ANAs, each of which binds to different proteins or protein complexes within the nucleus. According to this table, what kind of ANAs would be found in patients with SLE?`,
+      "question": `The images below represent the results of a urinalysis (indicator strip) and urine microscopy for a patient experiencing fatigue, frequent urination, and high blood pressure.  Use the Urinalysis Table and Urine Microscopy Table (handouts) to explain whether or not you would diagnose this patient with glomerulonephritis.`,
       "html": `
-        <p>Systemic Lupus Erythematosus (SLE, or "lupus") is an autoimmune disease characterized by the presence of large amounts of autoantibodies (i.e. antibodies that bind to self antigens) that circulate throughout the body.  The autoantibodies in lupus are called "antinuclear antibodies" (ANA) because they bind to the contents of a cell's nucleus.  As shown in the autoantibodies table (handout), there are many subtypes of ANAs, each of which binds to different proteins or protein complexes within the nucleus:</p>
-        <p>According to this table, what kind of ANAs would be found in patients with SLE?</p>
-        <textarea placeholder="Enter your text here"></textarea>`
-    },
-    {
-      "number": 6,
-      "kind": "write",
-      "name": "",
-      "thumbnail": "reports/imgs/immunology_question5.png",
-      "question": `The presence of ANAs in blood can be confirmed using a screening test. Following the positive detection of ANAs, the various subtypes of ANAs can be determined.  Based on the ANA test results shown below, what would be your diagnosis for this patient?`,
-      "html": `
-        <p>The presence of ANAs in blood can be confirmed using a screening test. Following the positive detection of ANAs, the various subtypes of ANAs can be determined.  Based on the ANA test results shown below, what would be your diagnosis for this patient?</p>
-        <img src="reports/imgs/immunology_question5.png"></img>
+        <p>The images below represent the results of a urinalysis (indicator strip) and urine microscopy for a patient experiencing fatigue, frequent urination, and high blood pressure.  Use the Urinalysis Table and Urine Microscopy Table (handouts) to explain whether or not you would diagnose this patient with glomerulonephritis.</p>
+        <img src="reports/imgs/nephrology_question4.jpg"></img>
         <textarea placeholder="Enter your text here"></textarea>`
     }
   ]
