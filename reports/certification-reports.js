e1f79dcaa4a908cda930b89fe85bb65a0c7b4b1a
megseoh <ohara.meagan@gmail.com>
2017-03-15 13:06:40 -0400

    added endocrinology content

--- reports/certification-reports.js
+++ reports/certification-reports.js
@@ -202,63 +202,56 @@ Skeletor.Mobile.certificationReports = [
     {
       "number": 1,
       "kind": "read",
-      "name": "Immunology Certification Questions",
+      "name": "Endocrinology Certification Questions",
       "thumbnail": "",
       "html": `
-        <h2>Immunology Certification Questions</h2>
-        <p>Working as a team, answer all of the following five questions correctly to earn your certificate of specialization in immunology.</p>`
+        <h2>Endocrinology Certification Questions</h2>
+        <p>Working as a team, answer all of the following four questions correctly to earn your certificate of specialization in endocrinology.</p>`
     },
     {
       "number": 2,
       "kind": "write",
       "name": "",
-      "thumbnail": "",
-      "question": `Explain how an inflammatory response works, including how it is triggered. Is this an example of innate immunity or adaptive immunity?`,
+      "thumbnail": "reports/imgs/endocrinology_question1.jpg",
+      "question": `Using the diagram below, identify the major endocrine glands and organs containing endocrine cells in humans.`,
       "html": `
-        <p>Explain how an inflammatory response works, including how it is triggered. Is this an example of innate immunity or adaptive immunity?</p>
+        <p>Using the diagram below, identify the major endocrine glands and organs containing endocrine cells in humans.</p>
+        <img src="reports/imgs/endocrinology_question1.jpg"></img>
         <textarea placeholder="Enter your text here"></textarea>`
     },
     {
       "number": 3,
       "kind": "write",
       "name": "",
-      "thumbnail": "reports/imgs/immunology_question2.png",
-      "question": `Use the diagram below to explain the role of helper T cells in mobilizing both the humoral and cell-mediated branches of the immune response`,
+      "thumbnail": "reports/imgs/endocrinology_question2.jpg",
+      "question": `Compare and contrast the two mechanisms of chemical signalling for a water-soluble hormone and a lipid-soluble hormone.`,
       "html": `
-        <p>Use the diagram below to explain the role of helper T cells in mobilizing both the humoral and cell-mediated branches of the immune response:</p>
-        <img src="reports/imgs/immunology_question2.png"></img>
+        <p>Compare and contrast the two mechanisms of chemical signalling for a water-soluble hormone and a lipid-soluble hormone.</p>
+        <img src="reports/imgs/endocrinology_question2.jpg"></img>
         <textarea placeholder="Enter your text here"></textarea>`
     },
     {
       "number": 4,
       "kind": "write",
       "name": "",
-      "thumbnail": "",
-      "question": `How is the immune system able to distinguish self from non-self?  What happens if it fails to do so?`,
+      "thumbnail": "reports/imgs/endocrinology_question3.jpg",
+      "question": `Explain how the feedback control loops shown in the diagram below are used to regulate the levels of TRH (TSH-releasing hormone), TSH (thyroid stimulating hormone), T3 (triiodothyronine) and T4 (tetraiodothyronine). Compare and contrast how the levels of TRH, TSH, T3 and T4 would differ in a patient with hyperthyroidism vs. hypothyroidism.`,
       "html": `
-        <p>How is the immune system able to distinguish self from non-self?  What happens if it fails to do so?</p>
+        <p>Explain how the feedback control loops shown in the diagram below are used to regulate the levels of TRH (TSH-releasing hormone), TSH (thyroid stimulating hormone), T3 (triiodothyronine) and T4 (tetraiodothyronine).</p>
+        <img src="reports/imgs/endocrinology_question3.jpg"></img>
+        <textarea placeholder="Enter your text here"></textarea>
+        <p>Compare and contrast how the levels of TRH, TSH, T3 and T4 would differ in a patient with hyperthyroidism vs. hypothyroidism.</p>
         <textarea placeholder="Enter your text here"></textarea>`
     },
     {
       "number": 5,
       "kind": "write",
       "name": "",
-      "thumbnail": "",
-      "question": `Systemic Lupus Erythematosus (SLE, or "lupus") is an autoimmune disease characterized by the presence of large amounts of autoantibodies (i.e. antibodies that bind to self antigens) that circulate throughout the body.  The autoantibodies in lupus are called "antinuclear antibodies" (ANA) because they bind to the contents of a cell's nucleus.  As shown in the autoantibodies table (handout), there are many subtypes of ANAs, each of which binds to different proteins or protein complexes within the nucleus. According to this table, what kind of ANAs would be found in patients with SLE?`,
+      "thumbnail": "reports/imgs/endocrinology_question4.jpg",
+      "question": `Grave's Disease is a form of hyperthyroidism in which an antibody called TRAb (TSH receptor antibody) mimics the TSH that is secreted by the anterior pituitary, thereby stimulating the thyroid gland to produce T3 and T4.  One method of testing for Grave's disease is to measure the levels of TSH and thyroid hormones in the bloodstream.  Given the blood test results below, explain why you would/would not diagnose this patient with Grave's Disease.`,
       "html": `
-        <p>Systemic Lupus Erythematosus (SLE, or "lupus") is an autoimmune disease characterized by the presence of large amounts of autoantibodies (i.e. antibodies that bind to self antigens) that circulate throughout the body.  The autoantibodies in lupus are called "antinuclear antibodies" (ANA) because they bind to the contents of a cell's nucleus.  As shown in the autoantibodies table (handout), there are many subtypes of ANAs, each of which binds to different proteins or protein complexes within the nucleus:</p>
-        <p>According to this table, what kind of ANAs would be found in patients with SLE?</p>
-        <textarea placeholder="Enter your text here"></textarea>`
-    },
-    {
-      "number": 6,
-      "kind": "write",
-      "name": "",
-      "thumbnail": "reports/imgs/immunology_question5.png",
-      "question": `The presence of ANAs in blood can be confirmed using a screening test. Following the positive detection of ANAs, the various subtypes of ANAs can be determined.  Based on the ANA test results shown below, what would be your diagnosis for this patient?`,
-      "html": `
-        <p>The presence of ANAs in blood can be confirmed using a screening test. Following the positive detection of ANAs, the various subtypes of ANAs can be determined.  Based on the ANA test results shown below, what would be your diagnosis for this patient?</p>
-        <img src="reports/imgs/immunology_question5.png"></img>
+        <p>Grave's Disease is a form of hyperthyroidism in which an antibody called TRAb (TSH receptor antibody) mimics the TSH that is secreted by the anterior pituitary, thereby stimulating the thyroid gland to produce T3 and T4.  One method of testing for Grave's disease is to measure the levels of TSH and thyroid hormones in the bloodstream.  Given the blood test results below, explain why you would/would not diagnose this patient with Grave's Disease.</p>
+        <img src="reports/imgs/endocrinology_question4.jpg"></img>
         <textarea placeholder="Enter your text here"></textarea>`
     }
   ]
