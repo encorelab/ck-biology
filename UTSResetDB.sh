53adbb1ea4666ebf4826812a04d8a56d28030942
Colin <colindmccann@gmail.com>
2017-03-31 00:08:25 +0700

    added scaf and a new reset script

--- UTSResetDB.sh
+++ UTSResetDB.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 # run this on the server in the ck-biology directory to clean up the terms after use
 
-# mongo ck-biology-SBI4UE-01-U4 --eval "db.users.remove()"
-# mongoimport -d ck-biology-SBI4UE-01-U4 -c users --jsonArray scaffolding/SBI4UE-01-U4-users.json
+mongo ck-biology-SBI4UE-01-U5 --eval "db.users.remove()"
+mongoimport -d ck-biology-SBI4UE-01-U5 -c users --jsonArray scaffolding/SBI4UE-01-U5-users.json
 
-# mongo ck-biology-SBI4UE-01-U4 --eval "db.lessons.remove()"
-# mongoimport -d ck-biology-SBI4UE-01-U4 -c lessons --jsonArray scaffolding/lessons-unit4.json
+mongo ck-biology-SBI4UE-01-U5 --eval "db.lessons.remove()"
+mongoimport -d ck-biology-SBI4UE-01-U5 -c lessons --jsonArray scaffolding/lessons-unit5.json
 
-# mongo ck-biology-SBI4UE-01-U4 --eval "db.terms.remove()"
-# mongoimport -d ck-biology-SBI4UE-01-U4 -c terms --jsonArray scaffolding/SBI4UE-01-U4-terms.json
+mongo ck-biology-SBI4UE-01-U5 --eval "db.terms.remove()"
+mongoimport -d ck-biology-SBI4UE-01-U5 -c terms --jsonArray scaffolding/SBI4UE-01-U5-terms.json
 
-# mongo ck-biology-SBI4UE-01-U4 --eval "db.relationships.remove()"
-# mongoimport -d ck-biology-SBI4UE-01-U4 -c relationships --jsonArray scaffolding/SBI4UE-01-U4-relationships.json
+mongo ck-biology-SBI4UE-01-U5 --eval "db.relationships.remove()"
+mongoimport -d ck-biology-SBI4UE-01-U5 -c relationships --jsonArray scaffolding/SBI4UE-01-U5-relationships.json
 
 # mongo ck-biology-SBI4UE-01-U4 --eval "db.articles.remove()"
 # mongoimport -d ck-biology-SBI4UE-01-U4 -c articles --jsonArray scaffolding/SBI4UE-01-U4-articles.json
@@ -20,17 +20,17 @@
 
 # mongo ck-biology-SBI4UE-01-U4 --eval "db.reports.remove()"
 
-# mongo ck-biology-SBI4UE-02-U4 --eval "db.users.remove()"
-# mongoimport -d ck-biology-SBI4UE-02-U4 -c users --jsonArray scaffolding/SBI4UE-02-U4-users.json
+mongo ck-biology-SBI4UE-02-U5 --eval "db.users.remove()"
+mongoimport -d ck-biology-SBI4UE-02-U5 -c users --jsonArray scaffolding/SBI4UE-02-U5-users.json
 
-# mongo ck-biology-SBI4UE-02-U4 --eval "db.lessons.remove()"
-# mongoimport -d ck-biology-SBI4UE-02-U4 -c lessons --jsonArray scaffolding/lessons-unit4.json
+mongo ck-biology-SBI4UE-02-U5 --eval "db.lessons.remove()"
+mongoimport -d ck-biology-SBI4UE-02-U5 -c lessons --jsonArray scaffolding/lessons-unit5.json
 
-# mongo ck-biology-SBI4UE-02-U4 --eval "db.terms.remove()"
-# mongoimport -d ck-biology-SBI4UE-02-U4 -c terms --jsonArray scaffolding/SBI4UE-02-U4-terms.json
+mongo ck-biology-SBI4UE-02-U5 --eval "db.terms.remove()"
+mongoimport -d ck-biology-SBI4UE-02-U5 -c terms --jsonArray scaffolding/SBI4UE-02-U5-terms.json
 
-# mongo ck-biology-SBI4UE-02-U4 --eval "db.relationships.remove()"
-# mongoimport -d ck-biology-SBI4UE-02-U4 -c relationships --jsonArray scaffolding/SBI4UE-02-U4-relationships.json
+mongo ck-biology-SBI4UE-02-U5 --eval "db.relationships.remove()"
+mongoimport -d ck-biology-SBI4UE-02-U5 -c relationships --jsonArray scaffolding/SBI4UE-02-U5-relationships.json
 
 # mongo ck-biology-SBI4UE-02-U4 --eval "db.articles.remove()"
 # mongoimport -d ck-biology-SBI4UE-02-U4 -c articles --jsonArray scaffolding/encore-U4-articles.json
